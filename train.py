cec9133ef11ae55cd6304f1ac97e3281c8b8b45d
Pratyush Maini <pratyus2@fac-lipton03.tepper.cmu.edu>
2023-10-11 15:46:29 -0400

    only local loss

--- train.py
+++ train.py
@@ -216,6 +216,7 @@ if __name__ == "__main__":
     )
     parser.add_argument("--grad_clip_norm", type=float, default=None)
     parser.add_argument("--save_frequency", type=int, default=1)
+    parser.add_argument("--only_local_loss", type=int, default=0)
 
     args = parser.parse_args()
     data_dir = args.data_dir
@@ -278,6 +279,8 @@ if __name__ == "__main__":
         f"{args.log_every_n_steps}",
         "--resume",
         f"{args.resume}",
+        "--only-local-loss",
+        f"{args.only_local_loss}",
     ]
     main_args.append("--dataset-resampled")
     if args.report_to_wandb:
