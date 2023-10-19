a788aaa8e5296c493f4f1879832f2985b18cf05c
SachinG007 <sach.goyalsachin@gmail.com>
2023-10-19 17:03:45 -0400

    preprocess text

--- eval_script.sh
+++ eval_script.sh
@@ -5,15 +5,21 @@ folder_path=$1
 # Sort files based on step number and iterate through them
 pids=()
 count=0
-for file in $(ls $folder_path | sort -n -k2 -t_ -); do
+# ckpt_folder_path="$folder_path/checkpoints/"
+for file in $(ls $folder_path/checkpoints | grep '\.pt$' | sort -n -k2 -t_ -); do
     ckpt_path="$folder_path/checkpoints/$file"
+    echo "folder path is below"
+    echo $folder_path
+    
+    echo "ckpt path is below"
+    echo $ckpt_path
     
     # Run the evaluation script
-    python "evaluate.py --train_output_dir $folder_path --use_model \"ViT-B-32 $ckpt_path\""
+    CUDA_VISIBLE_DEVICES=$count python evaluate.py --train_output_dir $folder_path --use_model \"ViT-B-32 $ckpt_path\" --data_dir /home/sachingo/
     
     pids+=($!)
     count=$((count+1))
-    if [ $count -eq 8 ]; then
+    if [ $count -eq 1 ]; then
         wait "${pids[@]}"
         
         # Reset counter and PIDs
