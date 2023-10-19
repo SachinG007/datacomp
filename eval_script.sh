76f8939458ebef6f481b5ae253792ddc6e33d214
SachinG007 <sach.goyalsachin@gmail.com>
2023-10-19 16:01:48 -0400

    eval updates

--- eval_script.sh
+++ eval_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-folder_path="/home/sachingo/datacomp/logs/clipbucket_30p_to_40p/checkpoints"
+folder_path=$1
 
 # Sort files based on step number and iterate through them
 for file in $(ls $folder_path | sort -n -k2 -t_ -); do
-	ckpt_path="$folder_path/$file"
-	    
-	# Run the evaluation script
-	echo "python evaluate.py --train_output_dir logs/clipbucket_30p_to_40p/ --use_model \"ViT-B-32 $ckpt_path\""
+    ckpt_path="$folder_path/checkpoints/$file"
+    
+    # Run the evaluation script
+    echo "python evaluate.py --train_output_dir $folder_path --use_model \"ViT-B-32 $ckpt_path\""
 done
