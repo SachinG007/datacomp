d076c5c286a2567aa8115d387664007cd2bebed3
SachinG007 <sach.goyalsachin@gmail.com>
2023-10-19 16:02:33 -0400

    eval updates

--- eval_script.sh
+++ eval_script.sh
@@ -7,5 +7,5 @@ for file in $(ls $folder_path | sort -n -k2 -t_ -); do
     ckpt_path="$folder_path/checkpoints/$file"
     
     # Run the evaluation script
-    echo "python evaluate.py --train_output_dir $folder_path --use_model \"ViT-B-32 $ckpt_path\""
+    python evaluate.py --train_output_dir $folder_path --use_model \"ViT-B-32 $ckpt_path\"
 done
