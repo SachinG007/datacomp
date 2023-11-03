a377daf6aec20362b5868419332243492730a22d
SachinG007 <sach.goyalsachin@gmail.com>
2023-11-03 16:48:44 -0400

    curr adt

---
+++ curriculum_5x_adt.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+declare -A path_epochs_dict
+
+paths_list=("/drive2/datacomp_L14_0.5_to_0.6::/drive2/datacomp_L14_0.4_to_0.5::/drive2/datacomp_L14_0.3_to_0.4::/drive2/datacomp_L14_0.1_to_0.2::/drive2/datacomp_L14_0.2_to_0.3::/drive2/datacomp_L14_0.0_to_0.1" "/drive2/datacomp_L14_0.4_to_0.5::/drive2/datacomp_L14_0.3_to_0.4::/drive2/datacomp_L14_0.1_to_0.2::/drive2/datacomp_L14_0.2_to_0.3::/drive2/datacomp_L14_0.0_to_0.1" "/drive2/datacomp_L14_0.3_to_0.4::/drive2/datacomp_L14_0.1_to_0.2::/drive2/datacomp_L14_0.2_to_0.3::/drive2/datacomp_L14_0.0_to_0.1" "/drive2/datacomp_L14_0.1_to_0.2::/drive2/datacomp_L14_0.2_to_0.3::/drive2/datacomp_L14_0.0_to_0.1" "/drive2/datacomp_L14_0.2_to_0.3::/drive2/datacomp_L14_0.0_to_0.1")
+epochs_list=(24 34 42 48 50)
+length=${#paths_list[@]}
+
+exp_name=clipbucket_curriculum_adt_128M_5x
+mkdir -p /home/pratyus2/logs/$exp_name/intermediate
+
+# Loop through the lists and print the elements
+for ((i = 0; i < length; i++)); do
+    datapath="${paths_list[i]}"
+    epochs="${epochs_list[i]}"
+    
+    echo $datapath
+    echo $epochs
+    
+    torchrun --master_port=12132 --nproc_per_node 8 train.py --scale medium_5x --data_dir $datapath --output_dir /home/pratyus2/logs/ --exp_name $exp_name --filter none --workers 10 --num_saves_per_epoch 1 --num_checkpoints 50 --curriculum 1 --resume latest --curriculum_epoch $epochs
+    
+    # move all the checkpoints to a folder stepwise if they have the term "step"
+    mv /home/pratyus2/logs/$exp_name/checkpoints/*step* /home/pratyus2/logs/$exp_name/intermediate/
+done
