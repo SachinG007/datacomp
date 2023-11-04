e44ce98bb8ae01575a722c57780eac749fbbd907
SachinG007 <sach.goyalsachin@gmail.com>
2023-11-03 20:01:19 -0400

    curr

--- train.py
+++ train.py
@@ -211,7 +211,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--log_every_n_steps",
         type=int,
-        default=100,
+        default=10,
         help="Log every n steps to tensorboard/console/wandb.",
     )
     parser.add_argument(
